80f8fbc3286d1c87d88b811ffb87b3ede5557d34
Michael Bazzinotti <mbazzinotti@gmail.com>
2015-07-23 10:01:01 -0400

    [oam-viewer] implement sprite-viewer. Default to 2X zoom

--- bsnes/ui-qt/debugger/ppu/oam-viewer.cpp
+++ bsnes/ui-qt/debugger/ppu/oam-viewer.cpp
@@ -24,7 +24,9 @@ void OamViewer::refresh() {
 
     //TODO: create method to expose ChipDebugger::property values by name
     unsigned width, height;
-    switch(0 /*SNES::ppu.oam_base_size()*/) { default:
+    switch(SNES::ppu.regs.oam_basesize)
+    { 
+      default:
       case 0: width = !size ?  8 : 16; height = !size ?  8 : 16; break;
       case 1: width = !size ?  8 : 32; height = !size ?  8 : 32; break;
       case 2: width = !size ?  8 : 64; height = !size ?  8 : 64; break;
@@ -35,6 +37,8 @@ void OamViewer::refresh() {
       case 7: width = !size ? 16 : 32; height = !size ? 32 : 32; break;
     }
 
+    //fprintf(stderr, "%d: w = %d, h = %d\n", v, width, height);
+
     signed xpos = (x << 8) + d0;
     if(xpos > 256) xpos = sclip<9>(xpos);
     unsigned ypos = d1;
@@ -42,9 +46,22 @@ void OamViewer::refresh() {
     unsigned priority = (d3 >> 4) & 3;
     unsigned palette = (d3 >> 1) & 7;
     string flags;
-    if(d3 & 0x80) flags << "V";
-    if(d3 & 0x40) flags << "H";
-    if(d3 & 0x01) flags << "N";
+    bool use_name=false, hflip=false, vflip=false;
+    if(d3 & 0x80)
+    {
+      flags << "V";
+      vflip = true;
+    }
+    if(d3 & 0x40)
+    {
+      flags << "H";
+      hflip = true;
+    }
+    if(d3 & 0x01)
+    {
+      flags << "N";
+      use_name = true;
+    }
 
     item->setText(1, string() << width << "x" << height);
     item->setText(2, string() << xpos);
@@ -53,8 +70,55 @@ void OamViewer::refresh() {
     item->setText(5, string() << priority);
     item->setText(6, string() << palette);
     item->setText(7, flags);
-  }
 
+    // Use the character (Tile#) based from OBJ VRAM location
+    // $2101 and PPU Appendix 1,2 are very useful to understandin OAM
+    fprintf(stderr, "cur_item = %d\n", atoi(list->currentItem()->text(0).toUtf8().data()));
+    fprintf(stderr, "OAM VRAM addr = $%X, name sel = $%X\n", SNES::ppu.regs.oam_tdaddr, SNES::ppu.regs.oam_nameselect);
+    if (list->currentItem() == item)
+    {
+      fprintf(stderr, "item %d matches!\n", v);
+      // Update the image
+      unsigned tile_width = width / 8;
+      unsigned tile_height = height / 8;
+      const uint8_t *source = SNES::memory::vram.data();
+      source += SNES::ppu.regs.oam_tdaddr;
+      source += use_name ? (SNES::ppu.regs.oam_nameselect+1) * (0x2000) : 0;
+      source += character * 0x20; // 4bpp tile size
+      uint32_t *dest = (uint32_t*)canvas->image->bits();
+
+      unsigned ppy,ppx;
+      for(unsigned ty = 0; ty < tile_height; ty++) 
+      {
+        for(unsigned tx = 0; tx < tile_width; tx++) 
+        {
+          for(unsigned py = ppy = 0; py < 8; py++,ppy+=2) 
+          {
+            uint8_t d0 = source[ 0];
+            uint8_t d1 = source[ 1];
+            uint8_t d2 = source[16];
+            uint8_t d3 = source[17];
+            for(unsigned px = ppx = 0; px < 8; px++,ppx+=2) 
+            {
+              uint8_t pixel = 0;
+              pixel |= (d0 & (0x80 >> px)) ? 1 : 0;
+              pixel |= (d1 & (0x80 >> px)) ? 2 : 0;
+              pixel |= (d2 & (0x80 >> px)) ? 4 : 0;
+              pixel |= (d3 & (0x80 >> px)) ? 8 : 0;
+              pixel *= 0x11;
+              dest[(ty * 8 + ppy) * 128 + (tx * 8 + ppx)] = (pixel << 16) + (pixel << 8) + pixel;
+              dest[(ty * 8 + ppy) * 128 + (tx * 8 + ppx + 1)] = (pixel << 16) + (pixel << 8) + pixel;
+              dest[(ty * 8 + ppy + 1) * 128 + (tx * 8 + ppx)] = (pixel << 16) + (pixel << 8) + pixel;
+              dest[(ty * 8 + ppy + 1) * 128 + (tx * 8 + ppx + 1)] = (pixel << 16) + (pixel << 8) + pixel;
+            }
+            source += 2;
+          }
+          source += 16;
+        }
+      }
+    }
+  }
+  
   for(unsigned i = 0; i <= 7; i++) list->resizeColumnToContents(i);
   canvas->update();
 }
